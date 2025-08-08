2e52d3ea9e9612055442027ba1bcfbcd603c9396
Fernando :) <119638429+Fernando-1909@users.noreply.github.com>
2025-08-08 16:10:59 -0300

    Update aviso.js

--- commands/aviso.js
+++ commands/aviso.js
@@ -1,19 +1,17 @@
-import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
-export const data = new SlashCommandBuilder()
-  .setName('aviso')
-  .setDescription('Envia um aviso para o servidor.')
-  .addStringOption(option =>
-    option.setName('mensagem')
-      .setDescription('Conteúdo do aviso')
-      .setRequired(true))
-  .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('aviso')
+        .setDescription('Envia um aviso para o servidor.')
+        .addStringOption(option =>
+            option.setName('mensagem')
+                .setDescription('Conteúdo do aviso')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
-export async function execute(interaction) {
-  const mensagem = interaction.options.getString('mensagem');
-
-  await interaction.reply({
-    content: `@everyone 📢 **Aviso:** ${mensagem}`,
-    allowedMentions: { parse: ['everyone'] }
-  });
-}
+    async execute(interaction) {
+        const mensagem = interaction.options.getString('mensagem');
+        await interaction.reply({ content: `📢 Aviso: ${mensagem}`, allowedMentions: { parse: [] } });
+    }
+};
