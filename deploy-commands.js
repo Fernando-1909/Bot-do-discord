0377d3853216b668fe22bf8c6ad9ba6a4b19cfb2
Fernando :) <119638429+Fernando-1909@users.noreply.github.com>
2025-08-08 15:55:40 -0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { REST, Routes } = require('discord.js');
 const fs = require('fs');
 
@@ -14,13 +15,13 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
     try {
-        console.log('🔄 Registrando comandos...');
+        console.log('🔄 Registrando comandos na guilda...');
         await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands }
         );
         console.log('✅ Comandos registrados com sucesso!');
     } catch (error) {
-        console.error(error);
+        console.error('❌ Erro ao registrar comandos:', error);
     }
 })();
